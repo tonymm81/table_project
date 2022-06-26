bbe63c2a30faeaf32d3e471eb0fe77a1b8bb146f
toni mäenpää <tonimaenpaa@kamk.fi>
2022-06-26 18:05:03 +0300

    ver102

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ ver101
 limit switches areworking well. software stops when the limit switch state turns 1. also motor powering is working well. uln chip works well.
 i dont haveultrasonic sensor yetbutthey are one rule also. user can set themeasurement to adjust the table. 
 but we continue the planning and developing
+
+bug:
+-up and down limit interrupt pin are not working as i hope. i have to figure out what is the problem here. I was ordering the better ultrasonic sensors from china
+i dont know why you cannot buy this versions from finland. That one what i buy from local shop was bad version. It demands own controlling board. 
+
+plan:
+perhaps inerruptpin has to check out twice is it true before it can give an order to stop the motor controlling. a also add graphical button where user
+can stop the motor. I also want to build up the radio button where user can choose table distanse or how much it gonna rise or going down.
+
+ver102
+added the slider where user can choose what is table distance from floor. limits are now working but i have to plan out how to control motor based on measurement.
--- main_program.py
+++ main_program.py
@@ -9,18 +9,20 @@ import RPi.GPIO as GP
 import sys
 import time
 import os
-import smbus
+#import smbus
 
-trigger = 4
+trigger = 7
 echo = 14
-relay_down_limit = 3
-relay_switch_direction = 2
+relay_down_limit = 8
+relay_switch_direction = 25
 relay_up_limit = 23
 relay_up = 15
-relay_down = 27
+relay_down = 12
 up_rule = 0
 down_rule = 0
 temp_value = 0
+level1 = 0
+what_device = ""
 
 GP.setwarnings(False)
 GP.setmode(GP.BCM)
@@ -43,8 +45,8 @@ root.geometry("880x500")
 root.configure(background="black")
 scrollbar = Scrollbar(root)
 scrollbar.pack( side = RIGHT, fill = Y )
-btn = Button(root, text="Adjust table up",fg="white", bg="black",font=("helvetica", 15), command=lambda: going_up()).pack() 
-btn1 = Button(root, text="Adjust table down",fg="white", bg="black",font=("helvetica", 15), command=lambda: going_down()).pack() 
+btn = Button(root, text="Adjust table up",fg="white", bg="black",font=("helvetica", 15), command=lambda: going_up(level1)).pack() 
+btn1 = Button(root, text="Adjust table down",fg="white", bg="black",font=("helvetica", 15), command=lambda: going_down(level1)).pack() 
 btn2 = Button(root, text="Control the lights and wlan plugs",fg="white", bg="black",font=("helvetica", 15), command=lambda: control_wlan_devices()).pack() 
 btn3 = Button(root, text="Save this setup",fg="white", bg="black",font=("helvetica", 15), command=lambda: save_setup()).pack() 
 btn3 = Button(root, text="Load setup",fg="white", bg="black",font=("helvetica", 15), command=lambda: load_setup()).pack() 
@@ -54,48 +56,95 @@ btn6 = Button(root, text="Check the updates",fg="white", bg="black",font=("helve
 label_1 = Label(root, text= "sais", font=("helvetica", 25), fg="white", bg="black")
 label_1.pack()
 #ultrasonic_ranger = trigger
+GP.add_event_detect(relay_up_limit, GP.RISING)
+GP.add_event_detect(relay_down_limit, GP.RISING)
+
+
+def ask_user(level, what_device):
+    level = DoubleVar()
+    
+    top1 = Toplevel()
+    top1.title('table measurement')
+    top1.geometry("400x300")
+    top1.configure(background="white")
+    top1.update()
+    l2 = Label(top1)
+    s2 = Scale( top1, variable = level,from_ = 50, to = 1,orient = VERTICAL)
+    s2.pack(anchor = CENTER)
+    print(level)
+    sel = "Vertical Scale Value = " + str(level.get())
+    l2.config(text = sel, font =("Courier", 14)) 
+     
+  
+    l4 = Label(top1, text = "set measurement")
+  
+    b2 = Button(top1, text ="choose measurement",
+            command = motor_up(1),
+            bg = "purple", 
+            fg = "white")
+  
+    btn = Button(top1, text="stop", fg="white",bg="black", font=("helvetica", 15), command=lambda: [motor_up(1), top1.destroy()]).pack()
+    
+     
+    l4.pack()
+    b2.pack()
+    l2.pack()
+    root.update()
+    level1 = level
+    top1.mainloop()
+    #top1.destroy()
+    return level1
 
 
 def motor_up(up_rule):
+    
     print("in motorfunction")
     if up_rule == 1:
         print("ei mennä ylös")
         GP.output(relay_up, GP.LOW)
+        GP.output(relay_switch_direction, GP.LOW)
         up_rule = 2
         
     
     if up_rule == 0:
         print("mennään ylös")
+        GP.output(relay_switch_direction, GP.HIGH)
         GP.output(relay_up, GP.HIGH)
-        #up_rule = 1
-            
         
     return up_rule
 
 
-def motor_down(down_rule):
+def motor_down(down_rule): # motor controlling down direction
     print("in motorfunction")
     if down_rule == 1:
         print("ei mennä alas")
         GP.output(relay_down, GP.LOW)
         down_rule = 2
-        
-    
+
     if down_rule == 0:
         print("mennään alas")
         GP.output(relay_down, GP.HIGH)
-        #down_rule = 1
-            
-        
+        #down_rule = 1   
+         
     return down_rule
  
-def going_up():
-    GP.add_event_detect(relay_up_limit, GP.RISING)
+def going_up(level1): #motor controlling up direction
+    what_device = "going_up(level)"
+    root.update()
+    level1 = ask_user(level1, what_device)
+    interrupt_counter = 0
+    print(level1)
     up_rule = 0
+    
     while True:
         print("are we really in loop")
+        #top.update()
         if GP.event_detected(relay_up_limit):
-            up_rule = 1
+                #up_rule = 1
+                #print("INERRUPT TO UP")
+            interrupt_counter = interrupt_counter +1
+            if interrupt_counter == 2:
+                up_rule = 1
 
         time.sleep(1)
         #up_rule = GP.input(relay_up_limit)
@@ -103,25 +152,37 @@ def going_up():
         up_rule = motor_up(up_rule)
 #         temp_value = up_rule   
         if up_rule == 2:
-            GP.remove_event_detect(relay_up_limit)
+            #GP.remove_event_detect(relay_up_limit)
             break
     return
  
  
-def going_down():
+def going_down(level1):
+    what_device = "going_down(level)"
+    top = Toplevel()
+    top.title('interrupt controlling')
+    top.configure(background="black")
+    lbl = Label(top, text="push button to stop",fg="white",bg="black", font=("helvetica", 20)).pack()
+    btn = Button(top, text="stop", fg="white",bg="black", font=("helvetica", 15), command=lambda: [motor_down(1), top.destroy()]).pack()
+    root.update()
+    level1 = ask_user(level1, what_device)
     down_rule = 0
-    GP.add_event_detect(relay_up_limit, GP.RISING)
+    interrupt_counter = 0
     while True:
-        if GP.event_detected(relay_up_limit):
-            down_rule = 1
+        top.update()
+        if GP.event_detected(relay_down_limit):# this doesnt need check twice option unlike going up needs
 
+            down_rule = 1
+            print("interrupt")
+            ##inerrupt_counter = interrupt_counter + 1
+            #if interrupt_counter >= 2:
+                #down_rule = 1
         print("are we really in loop")
         time.sleep(1)
-        #temp_value = GP.input(relay_down_limit)
-        print(temp_value)
         down_rule = motor_down(down_rule)
-        #temp_value = down_rule   
-        if down_rule == 2: 
+           
+        if down_rule == 2:
+            
             break
     return
  
@@ -135,13 +196,14 @@ def save_setup():
  
  
 def control_wlan_devices():
-     devices = broadlink.discover(timeout=5, local_ip_address='192.168.68.118')
-     print(devices)
-     return
+    root.update()
+    devices = broadlink.discover(timeout=5, local_ip_address='192.168.68.118')
+    print(devices)
+    return
  
  
 def measure_distance():
-    #val = ultrasonicRead(ultrasonic_ranger)
+    
     print("shit sensor")
     return
  
@@ -169,7 +231,10 @@ def measure_distance():
 
 def check_updates():
     return
- 
-measure_distance()
-main_waiting_loop()
-root.mainloop()
+
+while True:
+
+    measure_distance()
+    main_waiting_loop()
+    root.update()
+    time.sleep(1)
